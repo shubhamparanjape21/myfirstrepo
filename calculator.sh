924d842730c096b043ca15fe56b5c337c15f8797
Shubham <shubhamparanjape21@gmail.com>
2022-02-02 17:28:40 +0530

    added division feature

--- calculator.sh
+++ calculator.sh
@@ -9,6 +9,9 @@ sub=$(($num1-$num2))
 
 mul=$(($num1*$num2))
 
+div=$(($num1/$num2))
+
 echo "The addition of two numbers is: $add"
 echo "The subtraction of two numbers is: $sub"
 echo "The multiplication of two numbers is: $mul"
+echo "The division os two numbers is: $div"
